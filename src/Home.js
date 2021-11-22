16ddfdf5472535d4dfcddef6883c1f947a8f88fa
off2mxrs <off2mxrs@gmail.com>
2021-11-22 13:41:30 -0800

    Home.js setup

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hatchways</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
