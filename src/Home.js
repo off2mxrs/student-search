05a7047282b5f53e57683ad631827d0af0b7c05f
off2mxrs <off2mxrs@gmail.com>
2021-11-22 13:57:43 -0800

    data targetted

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,22 @@
 import React, { Component} from 'react';
+import axios from 'axios'
 
 class Home extends Component {
+    state = {
+        hatchways : []
+    }
+
+    componentDidMount() {
+        const url = 'https://api.hatchways.io/assessment/students'
+
+        axios.get(url)
+        .then( data => {
+            console.log(data);
+          this.setState({hatchways: data})
+          })
+          .catch(err => console.log(err))
+    }
+
     render() {
         return (
             <div>
