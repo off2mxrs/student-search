463e3e34a96a68472519e75f27875c15b66fff9a
off2mxrs <off2mxrs@gmail.com>
2021-11-23 22:04:30 -0800

    map complete

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,14 +5,13 @@ class Home extends Component {
     state = {
         hatchways: []
     }
-
     componentDidMount() {
         const url = 'https://api.hatchways.io/assessment/students'
 
         axios.get(url)
         .then( data => {
             
-            let arr = Object.entries(data)
+            let arr = Object.entries(data.data.students)
             console.log(arr);
             this.setState({
                 hatchways: arr
@@ -23,11 +22,11 @@ class Home extends Component {
     
     renderStudents() {
         return this.state.hatchways.map((student, idx) => {
-            // Object.values(student[1].students)
-            console.log(student[1].transitional);
+           
+            console.log(student[1].city);
             return (
                 <div key={idx}>
-                    {/* <p>{student[1].students}</p> */}
+                    <p>{student[1].firstName}</p>
                 </div>
             )
         })
