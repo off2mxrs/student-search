255ae126b63602b51719740be61fe4e0c7c7a8e7
off2mxrs <off2mxrs@gmail.com>
2021-12-01 10:31:18 -0800

    grades

--- src/Api.js
+++ src/Api.js
@@ -24,13 +24,37 @@ function Fetching() {
 
     const filteredStudents = getFilteredStudents(searchTerm, students)
     console.log(searchTerm)
+
+    function grades(arr) {
+        let sum = 0
+        let cnt = 0
+        let ave = 0
+        for (let i = 0; i < arr.length; i++) {
+            sum = sum + parseInt(arr[i]);
+            cnt++;
+        }
+        return ave = sum/cnt
+    }
+
     return (
         <div>
             <input className="search" type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
-            <ul>
-                {filteredStudents.map(student => <li key={student.id}>{student.firstName}</li>)}
-            </ul>
+            <div>
+             <ul className="list">
+                {filteredStudents.map(student => 
+                    <li key={student.id}><h1>{student.firstName} {student.lastName}</h1>
+                    <img src={student.pic}></img>
+                    <li>Email: {student.email} </li>
+                    <li>Company: {student.company}</li>
+                    <li>Skill: {student.skill}</li>
+                    <li>Average: {grades(student.grades)}%</li>
+                    </li>
+                   
+               )}
+             </ul>
+            </div>
         </div>
+
     )
 }
 
