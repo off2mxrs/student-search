b95d574d78d560db931f6caf7af5ff218e5b8ffd
off2mxrs <off2mxrs@gmail.com>
2021-11-30 21:17:46 -0800

    Search bar first attempt

--- src/Api.js
+++ src/Api.js
@@ -3,6 +3,7 @@ import axios from "axios"
 
 function Fetching() {
     const [students, setStudents] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
 
     useEffect(() => {
         axios.get('https://api.hatchways.io/assessment/students')
@@ -14,10 +15,21 @@ function Fetching() {
                 console.error(err))
     }, [])
 
+    const getFilteredStudents = (searchTerm, students) => {
+        if (!searchTerm) {
+            return students
+        }
+        return students.filter(info => info.firstName.includes(searchTerm))
+    }
+
+    const filteredStudents = getFilteredStudents(searchTerm, students)
+    console.log(searchTerm)
     return (
         <div>
+            <input className="search" type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
             <ul>
-                {students.map(student => <li key={student.id}>{student.firstName}</li>)}
+                {/* {students.map(student => <li key={student.id}>{student.firstName}</li>)} */}
+                {filteredStudents.map(student => <li key={student.id}>{student.firstName}</li>)}
             </ul>
         </div>
     )
--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import {useState} from 'react'
 import Api from './Api'
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
-
-  
+ 
   return (
     <div className="App">
-     <input className="search" type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
       <Api />
     </div>
   );
